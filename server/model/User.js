5204b0147ff78abbbe91ac447b674fd5454962fc
Utsav04 <utsavnagaria@gmail.com>
2022-05-14 21:12:26 +0530

    backend initial setup

---
+++ server/model/User.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.ObjectId
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+        },
+        username: String,
+        email: {
+            type: String,
+            required: true,
+            index: true,
+            unique: true
+        },
+        role: {
+            type: String,
+            default: "1",
+        },
+        Subscribe: {
+            type: String,
+            default: "0",
+        },
+        mobile_number: {
+            type: String,
+        },
+        password: {
+            type: String
+        },
+        dob: {
+            type: Date,
+        },
+        token: {
+            type: String,
+        },
+        validation: {
+            type: Boolean,
+            default: false,
+        },
+        signinToken: {
+            type: String
+        },
+        pic: {
+            type: String,
+            default: "https://res.cloudinary.com/quizee/image/upload/v1625900963/Profile/profile_gnpuwj.jpg"
+        },
+
+
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
